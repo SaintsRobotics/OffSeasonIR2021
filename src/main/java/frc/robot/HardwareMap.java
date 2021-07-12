39b148a10efbaaa917c11ae443b5dc2e1d2ddd98
TiaSinghania <sonicstormwind@gmail.com>
2021-07-12 15:02:12 -0700

    added shooter code from last time

---
+++ src/main/java/frc/robot/HardwareMap.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class HardwareMap {
+
+    public class ShooterHardware{
+        public CANSparkMax leftShooter = new CANSparkMax(16, MotorType.kBrushless );
+        public CANSparkMax rightShooter = new CANSparkMax(17, MotorType.kBrushless);
+        public SpeedControllerGroup shooter = new SpeedControllerGroup(leftShooter, rightShooter);
+        public ShooterHardware(){
+            rightShooter.setInverted(true);
+        }
+    };
+
+    public HardwareMap() {
+        
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.ShooterOffCommand;
+import frc.robot.commands.ShooterOnCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,9 +20,7 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private XboxController m_operatorController = new XboxController(1);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -34,7 +34,13 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    // turns on shooter when A is pressed
+    new JoystickButton(m_operatorController, Button.kA.value).whenPressed(new ShooterOnCommand());
+
+    // turns off shooter when B is pressed
+    new JoystickButton(m_operatorController, Button.kB.value).whenPressed(new ShooterOffCommand());
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -43,6 +49,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
