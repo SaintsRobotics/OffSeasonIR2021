e4cb5fd69b671009291d3c19ef3557220e49cffc
ProgrammingSR <programming@saintsrobotics.com>
2021-07-19 19:56:25 -0700

    Set shooter speed with a Bang Bang loop

--- src/main/java/frc/robot/commands/ShooterOnCommand.java
+++ src/main/java/frc/robot/commands/ShooterOnCommand.java
@@ -5,16 +5,19 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterOnCommand extends CommandBase {
   private ShooterSubsystem m_shooterSubsystem;
+  private PIDController m_pidController; 
   /** Creates a new ShooterCommand. */
   public ShooterOnCommand(ShooterSubsystem shooterSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_shooterSubsystem = shooterSubsystem;
+    m_pidController = new PIDController(0.5, 0, 0);
     addRequirements(m_shooterSubsystem);
   }
   Timer timer = new Timer();
@@ -29,14 +32,32 @@ public class ShooterOnCommand extends CommandBase {
 
     SmartDashboard.putNumber("Current Shooter Speed", m_shooterSubsystem.getShooterSpeed());
 
+    double currentSpeed = m_shooterSubsystem.getShooterSpeed();
+    if (currentSpeed < 0.8){
+      m_shooterSubsystem.setShooter(1);
+    }
+    
+    else if (currentSpeed > 0.8) {
+      m_shooterSubsystem.setShooter(0.5);
+    }
+
+    // m_pidController.setSetpoint(0.96);
+    // double pidOutput = m_pidController.calculate(m_shooterSubsystem.getShooterSpeed());
+    // if (pidOutput > 1) {
+    //   m_shooterSubsystem.setShooter(1);
+    // }
+    // m_shooterSubsystem.setShooter(pidOutput);
+    
     
 
-    if (timer.hasElapsed(4)) {
-      m_shooterSubsystem.setShooter(0.8);
+    
 
-    } else {
-      m_shooterSubsystem.setShooter(0.1);
-    }
+    // if (timer.hasElapsed(4)) {
+    //   m_shooterSubsystem.setShooter(0.8);
+
+    // } else {
+    //   m_shooterSubsystem.setShooter(0.1);
+    // }
         
   }
 
