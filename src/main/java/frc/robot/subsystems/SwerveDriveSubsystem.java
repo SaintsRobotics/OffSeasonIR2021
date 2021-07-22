331945c714af5195561b746fa2de9999be7ebe1e
delaynieym <delaynieym@outlook.com>
2021-07-21 21:30:42 -0700

    added normalize wheel speeds for swerve drivetrain
    tweaked javadocs. and auto-formatted.

--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -3,10 +3,12 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.HardwareMap;
 import frc.robot.HardwareMap.SwerveDriveHardware;
 
@@ -34,32 +36,41 @@ public class SwerveDriveSubsystem extends SubsystemBase {
     m_backLeftModule = swerveHardware.backLeft;
     m_backRightModule = swerveHardware.backRight;
 
-    m_kinematics = new SwerveDriveKinematics(m_frontLeftModule.getLocation(), m_frontRightModule.getLocation(), m_backLeftModule.getLocation(), m_backRightModule.getLocation());
+    m_kinematics = new SwerveDriveKinematics(m_frontLeftModule.getLocation(), m_frontRightModule.getLocation(),
+        m_backLeftModule.getLocation(), m_backRightModule.getLocation());
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
 
-    //Create chassisSpeed object
+    // Create chassisSpeed object
     m_chassisSpeeds = new ChassisSpeeds(m_xSpeed, m_ySpeed, m_rotationSpeed);
 
-    //toSwerveModuleState array create it from kinematics
+    // toSwerveModuleState array create it from kinematics
     SwerveModuleState[] swerveModuleStates;
     swerveModuleStates = m_kinematics.toSwerveModuleStates(m_chassisSpeeds);
-  
+
+    SwerveDriveKinematics.normalizeWheelSpeeds(swerveModuleStates,
+        Constants.SwerveConstants.MAX_SPEED_METERS_PER_SECOND);
 
     m_frontLeftModule.setDesiredState(swerveModuleStates[0]);
     m_frontRightModule.setDesiredState(swerveModuleStates[1]);
     m_backLeftModule.setDesiredState(swerveModuleStates[2]);
     m_backRightModule.setDesiredState(swerveModuleStates[3]);
   }
+
   /**
    * 
-   * @param xSpeed Represents forward velocity w.r.t the robot frame of reference. Radians per second (Forward is positive)
-   * @param ySpeed Represents sideways velocity w.r.t the robot frame of reference. Radians per second (Left is positive)
-   * @param rotationSpeed Represents the angular velocity of the robot frame. Radians per second (Counterclockwise is positive)
-   * @param isFieldRelative 
+   * @param xSpeed          Represents forward velocity w.r.t the robot frame of
+   *                        reference. Radians per second (Forward is positive)
+   * @param ySpeed          Represents sideways velocity w.r.t the robot frame of
+   *                        reference. Radians per second (Left is positive)
+   * @param rotationSpeed   Represents the angular velocity of the robot frame.
+   *                        Radians per second (Counterclockwise is positive)
+   * @param isFieldRelative Whether or not the provided x and y values should be
+   *                        considered relative to the field, or relative to the
+   *                        robot.
    */
   public void move(double xSpeed, double ySpeed, double rotationSpeed, boolean isFieldRelative) {
 
