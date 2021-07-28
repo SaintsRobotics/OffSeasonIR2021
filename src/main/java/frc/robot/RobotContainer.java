1707cc07f86a85de8d4c1ffc0129707bba8c993c
noam987 <noam@arie11.com>
2021-07-27 23:58:43 -0700

    added joystick command constructed Swerve objects

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,53 +14,64 @@ import frc.robot.commands.MoveArmCommand;
 import frc.robot.commands.ShootOneBallCommand;
 import frc.robot.commands.ShooterOffCommand;
 import frc.robot.commands.ShooterOnCommand;
+import frc.robot.commands.SwerveJoystickCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
-
+import frc.robot.subsystems.SwerveDrivetrain;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private XboxController m_driverController = new XboxController(0);
-  private XboxController m_operatorController = new XboxController(1);
+
   private HardwareMap m_hardwareMap = new HardwareMap();
   private ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem(m_hardwareMap.shooterHardware);
- 
+  private SwerveDrivetrain m_swerveDrivetrain = new SwerveDrivetrain(m_hardwareMap.swerveHardware);
+  private SwerveJoystickCommand m_swerveJoystickCommand = new SwerveJoystickCommand(m_swerveDrivetrain,
+      m_hardwareMap.driveController);
   private IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem(m_hardwareMap.intakeHardware);
-  private MoveArmCommand m_moveArmCommand = new MoveArmCommand(m_operatorController, m_intakeSubsystem);
-  
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private MoveArmCommand m_moveArmCommand = new MoveArmCommand(m_hardwareMap.opperatorController, m_intakeSubsystem);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
     m_intakeSubsystem.setDefaultCommand(m_moveArmCommand);
+    m_swerveDrivetrain.setDefaultCommand(m_swerveJoystickCommand);
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
 
     // turns on shooter when Left Bumper is pressed
-    new JoystickButton(m_operatorController, Button.kBumperLeft.value).whenPressed(new ShooterOnCommand(m_shooterSubsystem));
+    new JoystickButton(m_hardwareMap.opperatorController, Button.kBumperLeft.value)
+        .whenPressed(new ShooterOnCommand(m_shooterSubsystem));
     // turns off shooter when Right Bumper is pressed
-    new JoystickButton(m_operatorController, Button.kBumperRight.value).whenPressed(new ShooterOffCommand(m_shooterSubsystem));
+    new JoystickButton(m_hardwareMap.opperatorController, Button.kBumperRight.value)
+        .whenPressed(new ShooterOffCommand(m_shooterSubsystem));
     // runs intake forwards while X is held
-		new JoystickButton(m_operatorController, Button.kX.value).whenHeld(new IntakeCommand(m_intakeSubsystem));
-		// runs the intake backwards while Y is heldnew ShooterOnCommand(m_shooterSubsystem)
-		new JoystickButton(m_operatorController, Button.kY.value).whenHeld(new IntakeCommand(m_intakeSubsystem));
+    new JoystickButton(m_hardwareMap.opperatorController, Button.kX.value)
+        .whenHeld(new IntakeCommand(m_intakeSubsystem));
+    // runs the intake backwards while Y is heldnew
+    // ShooterOnCommand(m_shooterSubsystem)
+    new JoystickButton(m_hardwareMap.opperatorController, Button.kY.value)
+        .whenHeld(new IntakeCommand(m_intakeSubsystem));
     // runs the shoot one ball command while A is held
-    new JoystickButton(m_operatorController, Button.kA.value).whenHeld(new ShootOneBallCommand(m_shooterSubsystem));
-    
-  
+    new JoystickButton(m_hardwareMap.opperatorController, Button.kA.value)
+        .whenHeld(new ShootOneBallCommand(m_shooterSubsystem));
+
   }
 
   /**
---
+++ src/main/java/frc/robot/commands/SwerveJoystickCommand.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Utils;
+import frc.robot.subsystems.SwerveDrivetrain;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.ModuleConstants;
+
+/** Controls the drivetrain with an Xbox controller. */
+public class SwerveJoystickCommand extends CommandBase {
+	private SwerveDrivetrain m_drivetrain;
+	private XboxController m_controller;
+
+	/**
+	 * Creates a new {@link SwerveJoystickCommand}.
+	 * 
+	 * @param drivetrain The {@link SwerveDrivetrain} subsystem to use.
+	 */
+	public SwerveJoystickCommand(SwerveDrivetrain drivetrain, XboxController controller) {
+		addRequirements(drivetrain);
+		m_drivetrain = drivetrain;
+		m_controller = controller;
+
+	}
+
+	@Override
+	public void execute() {
+		// apply functions to controller values to 1) check deadzone 2) apply quadratic
+		// relation between controller/speed
+		double x = Utils.oddSquare(Utils.deadZones(-m_controller.getY(Hand.kLeft), 0.2))
+				* DriveConstants.MAX_SPEED_METERS_PER_SECOND * 0.2;
+		double y = Utils.oddSquare(Utils.deadZones(m_controller.getX(Hand.kLeft), 0.2))
+				* DriveConstants.MAX_SPEED_METERS_PER_SECOND * 0.2;
+		double rot = Utils.oddSquare(Utils.deadZones(-m_controller.getX(Hand.kRight), 0.2))
+				* ModuleConstants.MAX_MODULE_ANGULAR_SPEED_RADIANS_PER_SECOND * 0.2;
+
+		m_drivetrain.drive(x, y, rot, m_controller.getBumper(Hand.kRight));
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+		m_drivetrain.drive(0, 0, 0, false);
+	}
+}
