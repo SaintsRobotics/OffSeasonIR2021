36539d880ca881354f3f5169fa3f6c1a3e182510
delaynieym <delaynieym@outlook.com>
2021-08-16 21:01:12 -0700

    updated climb method docs

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -3,7 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems;
 
-
 import frc.robot.Constants;
 
 //test
@@ -25,12 +24,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.HardwareMap.ClimberHardware;
 
-public class ClimberSubsystem extends SubsystemBase{
+public class ClimberSubsystem extends SubsystemBase {
   public Servo m_servoMotor;
   public Servo m_ratchetServo;
   public CANSparkMax m_winchMotor;
-
-
 
   public ClimberSubsystem(ClimberHardware climberHardware) {
     m_servoMotor = climberHardware.servoMotor;
@@ -39,55 +36,68 @@ public class ClimberSubsystem extends SubsystemBase{
 
   }
 
-/**
- * The Angle of the Servo that releases the climber
- * @return
- */
+  /**
+   * The Angle of the Servo that releases the climber
+   * 
+   * @return
+   */
   public double getAngle() {
     return m_servoMotor.get();
   }
-/**
- * Sets the ratchet servo so that when the winch drives it drives backward (The Climber mechanism moves up)
- */
-  public void releaseRatchet() {
-      m_ratchetServo.set(Constants.ClimberConstants.WINCH_REVERSE_SERVO_POSITION);
-      SmartDashboard.putString("Ratchet State", "extend");
-      DriverStation.reportError("climb direction reversed", false);
-  }
+
   /**
-   * Sets the ratchet servo so that when the winch drives it drives forward (The Climber mechanism moves down)
+   * Sets the ratchet servo so that when the winch drives it drives backward (The
+   * Climber mechanism moves up)
+   */
+  public void releaseRatchet() {
+    m_ratchetServo.set(Constants.ClimberConstants.WINCH_REVERSE_SERVO_POSITION);
+    SmartDashboard.putString("Ratchet State", "extend");
+    DriverStation.reportError("climb direction reversed", false);
+  }
+
+  /**
+   * Sets the ratchet servo so that when the winch drives it drives forward (The
+   * Climber mechanism moves down)
    */
   public void lockRatchet() {
-      m_ratchetServo.set(Constants.ClimberConstants.WINCH_NORMAL_SERVO_POSITION);
-      SmartDashboard.putString("Ratchet State", "retract");
-      DriverStation.reportError("climb direction normal", false);
+    m_ratchetServo.set(Constants.ClimberConstants.WINCH_NORMAL_SERVO_POSITION);
+    SmartDashboard.putString("Ratchet State", "retract");
+    DriverStation.reportError("climb direction normal", false);
   }
-/**
- * Moves the Servo to release the Climber mechanism
- */
+
+  /**
+   * Moves the Servo to release the Climber mechanism
+   */
   public void releaseClimber() {
-      m_servoMotor.set(Constants.ClimberConstants.SERVO_RELEASE_POSITION);
-      SmartDashboard.putString("ClimberState", "Released");
-      DriverStation.reportError("climb released ", false);
+    m_servoMotor.set(Constants.ClimberConstants.SERVO_RELEASE_POSITION);
+    SmartDashboard.putString("ClimberState", "Released");
+    DriverStation.reportError("climb released ", false);
   }
-/**
- * Moves the servo to the position where the robot would be secured down
- */
+
+  /**
+   * Moves the servo to the position where the robot would be secured down
+   */
   public void lockServo() {
     m_servoMotor.set(Constants.ClimberConstants.SERVO_RETURN_POSITION);
   }
-/**
- * The speed of the winch
- * @param speed Can only input negative values between 0 and -1 pulls the robot up
- */
+
+  /**
+   * To bring the climber closer to the robot, input a value in [-1, 0]. To drive
+   * the winch in the opposite direction, input a value in [0, 1]. <b>MAKE SURE
+   * THE RACHET IS DISENGAGED BEFORE INPUTING NEGATIVE VALUES</b>, otherwise the
+   * motor will stall.
+   * 
+   * @param speed Speed of the winch motor.
+   */
   public void climb(double speed) {
-      m_winchMotor.set(speed);
+    m_winchMotor.set(speed);
   }
-/**
- * 
- * @return The speed of the winch motor
- */
+
+  /**
+   * 
+   * @return The speed of the winch motor
+   */
   public double getSpeed() {
-      return m_winchMotor.getEncoder().getVelocity();
+    return m_winchMotor.getEncoder().getVelocity();
   }
 }
