78ed9f62ed9abfa62d4ffc0df8c0bd4ea4e29ce4
delaynieym <delaynieym@outlook.com>
2021-09-04 19:07:17 -0700

    empty class w brief description for OperatorBoard

---
+++ src/main/java/frc/robot/OperatorBoard.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+/**
+ * A wrapper class for our custom-made operator board. Access Buttons via state.
+ * Access axis values via methods. Use methods to make buttons light up.
+ */
+public class OperatorBoard {
+
+}
