1115dd2c50e7ee76f2f03d42501b1aeb0a59d1b9
EliteCoder21 <emailaaryan@protonmail.com>
2021-09-26 17:02:57 -0700

    RPM prediction code completed

--- src/main/java/frc/robot/GetPrediction.java
+++ src/main/java/frc/robot/GetPrediction.java
@@ -1,24 +1,26 @@
 package frc.robot;
 
+
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.io.ClassPathResource;
 
+
 public class GetPrediction {
     
-    public static void main(String[] args) {
+    public static double getRPM(double[] pos) {
 
         // load the model
-        String simpleMlp = new ClassPathResource("games.h5").getFile().getPath();
+        String simpleMlp = new ClassPathResource("RPM_model.h5").getFile().getPath();
         MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights(simpleMlp);
 
         // make a random sample
         int inputs = 2;
         INDArray features = Nd4j.zeros(inputs);
         for (int i=0; i<inputs; i++) {
-            features.putScalar(new int[] {i}, Math.random() < 0.5 ? 0 : 1);
+            features.putScalar(new int[] {i}, pos[i]);
         }
         // get the prediction
         double prediction = model.output(features).getDouble(0);
