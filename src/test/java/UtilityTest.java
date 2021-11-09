8524be2affaad2fdda5794d5e89b73a9c5866c5f
elementalearth123 <hanchoof@outlook.com>
2021-11-08 16:37:45 -0800

    get tests setup and tests

---
+++ src/test/java/UtilityTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class UtilityTest {
+    @Test
+    public void loltest() {
+        assertEquals(1, 1, 0);
+    }
+
+}
